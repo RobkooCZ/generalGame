3e754147f3e1b629e1c6a4e2106825c9c3e5c4f3
RobkooCZ <eichlergm@gmail.com>
2024-05-14 12:09:15 +0200

    changed in file structure

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Header from "@/components/header"
+
+function Page (){
+  return (
+    <Header/>
+  )
+}
+
+export default Page;
---
+++ components/header.jsx
@@ -0,0 +1,11 @@
+import "@/public/css/titlePage.css"
+
+function Header (){
+    return (
+        <div className="header">
+            <h1>ZHUKOV SIMULATOR v1.0.0</h1>
+        </div>
+    )
+}
+
+export default Header;
